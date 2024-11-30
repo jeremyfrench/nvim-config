676059f790661df7714e61ff878016692a849278
Jeremy French <jeremy@jeremyfrench.co.uk>
2024-11-30 00:24:50 +0000

    Put keymap into plugin

--- lua/plugins/blame.lua
+++ lua/plugins/blame.lua
@@ -2,6 +2,7 @@ return {
   {
     "FabijanZulj/blame.nvim",
     lazy = false,
+    keys = { { "<leader>gW", "<cmd>BlameToggle<cr>", desc = "Git Blame File" } },
     config = function()
       require("blame").setup({})
     end,
